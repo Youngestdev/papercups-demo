820e35cf0f2f8e7abdeb05aba1af028ac83845d4
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2020-08-24 22:04:56 +0100

    Added widget

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ChatWidget from '@papercups-io/chat-widget'
 import { StaticQuery, graphql } from 'gatsby'
 import Header from "../components/header"
 import Footer from "../components/footer"
@@ -9,26 +10,37 @@ import "./layout.sass"
 const Layout = ({ children }) => {
     
     return (
-        <Provider>
-            <StaticQuery
-                query={graphql`
-                query SiteTitleQuery {
-                site {
-                    siteMetadata {
-                    title
+        <>
+            <Provider>
+                <StaticQuery
+                    query={graphql`
+                    query SiteTitleQuery {
+                    site {
+                        siteMetadata {
+                        title
+                        }
                     }
-                }
-                }
-            `}
-                render={data => (
-                    <>
-                        <Header siteTitle={data.site.siteMetadata.title} />
-                        {children}
-                        <Footer />
-                    </>
-                )}
+                    }
+                `}
+                    render={data => (
+                        <>
+                            <Header siteTitle={data.site.siteMetadata.title} />
+                            {children}
+                            <Footer />
+                        </>
+                    )}
+                />
+            </Provider>
+            <ChatWidget
+                title='Welcome to Dev9ine'
+                subtitle= 'Ask us anything in the chat window below 😊'
+                primaryColor='#1890ff'
+                greeting=''
+                newMessagePlaceholder='Start typing...'
+                accountId=''
+                baseUrl='https://app.papercups.io'
             />
-        </Provider>
+        </>
     );
 };
 
